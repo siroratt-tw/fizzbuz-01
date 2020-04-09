9f49bbd22bc6028001eb8ebee61cbcbda3be3491
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2020-04-09 11:52:00 +0700

    initial proj

---
+++ FizzBuzz/Calculator.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator.swift
+//  FizzBuzz
+//
+//  Created by Siroratt Suntronsuk on 9/4/2563 BE.
+//  Copyright © 2563 Siroratt Suntronsuk. All rights reserved.
+//
+
+struct Calculator {
+    func getText(with number: Int) -> String {
+        return ""
+    }
+}
+
+struct User {
+    <#fields#>
+}
---
+++ FizzBuzz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FizzBuzz
+//
+//  Created by Siroratt Suntronsuk on 9/4/2563 BE.
+//  Copyright © 2563 Siroratt Suntronsuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
